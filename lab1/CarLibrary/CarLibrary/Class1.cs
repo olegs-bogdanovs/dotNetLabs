b7b902f2a2ab52b0582d59a9542f1382e45bcfce
Olegs Bogdanovs <olegs.bogdanovs@dukascopy.com>
2019-01-14 15:28:46 +0200

    Add lab1

---
+++ lab1/CarLibrary/CarLibrary/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CarLibrary
+{
+    // Представляет состояние двигателя,
+    public enum EngineState
+    { engineAlive, engineDead }
+
+    // Абстрактный базовый класс в иерархии,
+    public abstract class Car
+    {
+        public string PetName { get; set; }
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        protected EngineState egnState = EngineState.engineAlive;
+        public EngineState EngineState
+        {
+            get { return egnState; }
+        }
+        public abstract void TurboBoost();
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            PetName = name; MaxSpeed = maxSp; CurrentSpeed = currSp;
+        }
+    }
+
+    public class SportsCar : Car
+    {
+        public SportsCar() { }
+        public SportsCar(string name, int maxSp, int currSp)
+        : base(name, maxSp, currSp) { }
+        public override void TurboBoost()
+        {
+            MessageBox.Show("Faster is b e t t e r . . . ", "Ramming speed!");
+        }
+    }
+
+    public class MiniVan : Car
+    {
+        public MiniVan() { }
+        public MiniVan(string name, int maxSp, int currSp)
+        : base(name, maxSp, currSp) { }
+        public override void TurboBoost()
+        {
+            // Минивэны имеют плохие возможности ускорения!
+            egnState = EngineState.engineDead;
+            MessageBox.Show("Your engine block exploded!", "Eek!");
+
+        }
+    }
+
+}
