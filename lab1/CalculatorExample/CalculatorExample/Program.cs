08596f678cbd79d5b4fe2bfdab0743d8f0eac672
Olegs Bogdanovs <bogdanovsolegs@gmail.com>
2019-01-14 21:04:31 +0200

    Completed Lab 1

---
+++ lab1/CalculatorExample/CalculatorExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorExample
+{
+    class Program
+    {
+        // Калькулятор на С#.
+        class Calc
+        {
+            public int Add(int x, int y) { return x + y; }
+        }
+        static void Main(string[] args)
+        {
+            Calc c = new Calc();
+            int ans = c.Add(10, 84);
+            Console.WriteLine("10 + 84 is {0}.", ans);
+            // Ожидать нажатия пользователем клавиши <Enter> перед выходом.
+            Console.ReadLine();
+        }
+    }
+}
