5606eba43e04e45b6ba96b7cde305f558ccd6ebc
Olegs Bogdanovs <olegs.bogdanovs@dukascopy.com>
2019-01-14 14:29:46 +0200

    Lab6 updated

---
+++ lab6/Generation_GC/Generation_GC/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generation_GC
+{
+    class Car
+    {
+        private int speed;
+        private string name;
+        public Car(string name, int speed)
+        {
+            this.name = name;
+            this.speed = speed;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} speed is {1} Km/h", name, speed);
+        }
+    }
+}
---
+++ lab6/Simple_GC/Simple_GC/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Simple_GC
+{
+    class Test
+    {
+        int[] arr = new int[1000000000];
+        public void Method(int i)
+        {
+            //Console.WriteLine(i);
+        }
+        ~Test()
+        {
+            //Console.WriteLine("Object " + this.GetHashCode() + "deleted");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tests = new Test[1000];
+            try
+            {
+                for (int i = 0; i < tests.Length; i++)
+                {
+                    Test test = new Test();
+                    tests[i] = test;
+                    //test.Method(i);
+                }
+            }
+            catch (OutOfMemoryException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Heap memory low");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            Console.ReadLine();
+        }
+    }
+}
