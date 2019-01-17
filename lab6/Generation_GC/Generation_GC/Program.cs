3a1e45e5e8a6ddbd0c4e5c1f15d392c9dbea7174
Olegs Bogdanovs <olegs.bogdanovs@dukascopy.com>
2019-01-17 17:02:03 +0200

    Complete Lab6

--- lab6/Generation_GC/Generation_GC/Program.cs
+++ lab6/Generation_GC/Generation_GC/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Threading;
 
 namespace Generation_GC
 {
     class Program
     {
+        private static void ShowGCStat()
+        {
+            Console.WriteLine("Generation 0 checked {0} time(s)",
+            GC.CollectionCount(0));
+            Console.WriteLine("Generation 1 checked {0} time(s)",
+            GC.CollectionCount(1));
+            Console.WriteLine("Generation 2 checked {0} time(s)",
+            GC.CollectionCount(2));
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Thread.Sleep(4000);
+            Console.WriteLine("Heap memory size in bytes : {0}",
+            GC.GetTotalMemory(false));
+            Console.WriteLine("System supports {0} generations",
+            GC.MaxGeneration + 1);
+
+            Car car = new Car("Volvo", 120);
+            Console.WriteLine(car.ToString());
+            Console.WriteLine("Object car relates to {0} generation",
+            GC.GetGeneration(car));
+            Console.WriteLine("Heap memory size in bytes : {0}",
+            GC.GetTotalMemory(false));
+
+            object[] arr = new object[10000000];
+            ShowGCStat();
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = new object();
+
+            Console.WriteLine("Heap memory size in bytes : {0}",
+            GC.GetTotalMemory(false));
+            arr = null;
+
+            Console.WriteLine("Array is created, start GC");
+            ShowGCStat();
+
+            var start = DateTime.Now;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Console.WriteLine("GC worked " + (DateTime.Now -
+            start).Milliseconds);
+            Console.WriteLine("Heap memory size in bytes : {0}",
+            GC.GetTotalMemory(false));
+            Console.WriteLine("Object car relates to {0} generation",
+            GC.GetGeneration(car));
+            ShowGCStat();
+
+            Console.ReadLine();        }
     }
 }
---
+++ lab6/TmerGC/TmerGC/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TmerGC
+{
+    class Program
+    {
+        static void Method(object state)
+        {
+            Console.WriteLine(state);
+            GC.Collect();
+        }
+        static void Main(string[] args)
+        {
+            var timer = new Timer(Method, "Hello", 0, 200);
+            Console.ReadLine();
+            timer.Dispose();
+        }
+    }
+}
